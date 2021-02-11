de933e09c3551c3458c6b8116ab32bcfa51c3124
Annie <komochek2013@gmail.com>
2021-02-11 16:43:16 +0300

    finish adaptive

--- src/js/sections/trainings.js
+++ src/js/sections/trainings.js
@@ -1,6 +1,34 @@
 import $ from 'jquery';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 $(document).ready(function () {
     $('.trainings__card_animated').hover(function () {
         $(this).toggleClass('trainings__card_animated');
     });
+});
+
+new Swiper('.trainings .swiper-container', {
+    direction: 'horizontal',
+    loop: false,
+
+    breakpoints: {
+        0: {
+            slidesPerView: 'auto',
+            centeredSlides: true,
+            spaceBetween: 30,
+            allowTouchMove: true,
+        },
+
+        936: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+            allowTouchMove: false,
+        },
+
+        1416: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+            allowTouchMove: false,
+        },
+    },
 });
